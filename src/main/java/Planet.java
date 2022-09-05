6ebbe8b08154de76c110ab5fbc5b615313da5fff
“lawriecarly” <Lawrie.davidson91@gmail.com>
2022-09-05 14:16:27 +0100

    Initial Commit

---
+++ src/main/java/Planet.java
@@ -0,0 +1,18 @@
+public class Planet {
+
+    private String name;
+    private double size;
+
+    public Planet(String name, double size){
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public double getSize(){
+        return this.size;
+    }
+}
---
+++ src/main/java/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+
+    public static void main(String[] args) {
+        Planet myPlanet;
+        myPlanet = new Planet("Earth", 100.5);
+        System.out.println(myPlanet.getName());
+    }
+}
